cffa87a856347ef1432a445c82aeff55abc7763b
Kadevsi <rafkienl@gmail.com>
2024-03-20 16:13:19 -0600

    Starting the  repo

---
+++ Models/Estudiante.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundamentos.Models
+{
+    public class Estudiante
+    {
+        int Id;
+
+        string Nombres;
+
+        string Email;
+
+        string Direccion;
+        string Telefono;
+
+        void MarcarAsistencia()
+        {
+
+
+        }
+
+
+    }
+}
---
+++ Models/Profesor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundamentos.Models
+{
+    public class Profesor
+
+    {
+
+        int Id;
+
+        string Nombres;
+
+        string Email;
+
+        string Direccion;
+        string Telefono;
+
+        void MarcarAsistencia()
+        {
+
+
+        }
+
+    }
+}
